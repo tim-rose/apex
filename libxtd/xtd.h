4ac012a669f7a243f71e6203c578579ea796bdef
Tim Rose <tim.rose@acm.org>
2021-08-09 19:49:19 +1000

    Adds MACRO_VALUE_STR()

--- libxtd/xtd.h
+++ libxtd/xtd.h
@@ -2,14 +2,16 @@
  * XTD.H --Funadamental macros and utilities.
  *
  * Contents:
- * MAX()       --Return the maximum of two numeric values.
- * MIN()       --Return the minimum of two numeric values.
- * ABS()       --Return the absolute of a numeric value.
- * IN_RANGE()  --Test if a value is within a specified range.
- * FEQUAL()    --Test if two float values are suitably close.
- * NEL()       --Return the number of elements in an array.
- * NEW()       --Allocate space for some items of a specified type.
- * SYS_RETRY() --a lame but portable version of GNU's TEMP_FAILURE_RETRY.
+ * MACRO_VALUE_STR() --Get the value of a macro as a text string.
+ * MACRO_ARG_STR()   --Get the value of a macro argument as a text string.
+ * MAX()             --Return the maximum of two numeric values.
+ * MIN()             --Return the minimum of two numeric values.
+ * ABS()             --Return the absolute of a numeric value.
+ * IN_RANGE()        --Test if a value is within a specified range.
+ * FEQUAL()          --Test if two float values are suitably close.
+ * NEL()             --Return the number of elements in an array.
+ * NEW()             --Allocate space for some items of a specified type.
+ * SYS_RETRY()       --a lame but portable version of GNU's TEMP_FAILURE_RETRY.
  *
  * See Also:
  * http://unixwiz.net/techtips/gnu-c-attributes.html
@@ -40,14 +42,46 @@
 #define LINE_MAX 2048
 #endif /* LINE_MAX */
 
+
+/*
+ * MACRO_VALUE_STR() --Get the value of a macro as a text string.
+ * MACRO_ARG_STR() --Get the value of a macro argument as a text string.
+ *
+ * Remarks:
+ * This macro is useful for creating size-limited `scanf()` formats:
+ * ```
+ * #define TEXT_SIZE 100
+ * char *first_word(const char *str)
+ * {
+ *     static char text[TEXT_SIZE+1];
+ *     static char format[] = "%" MACRO_VALUE_STR(TEXT_SIZE) "s";
+ *     if (sscanf(str, format, text) == 1)
+ *     {
+ *         return text;
+ *     }
+ *     else
+ *     {
+ *         return NULL;
+ *     }
+ * }
+ * ```
+ * See Also:
+ * https://gcc.gnu.org/onlinedocs/gcc-11.2.0/cpp/Stringizing.html
+ */
+#define MACRO_VALUE_STR(s) MACRO_ARG_STR(s)
+#define MACRO_ARG_STR(s) #s
+
 /*
  * MAX() --Return the maximum of two numeric values.
  * MIN() --Return the minimum of two numeric values.
  * ABS() --Return the absolute of a numeric value.
+ *
+ * Remarks:
+ * Arguments to these macros must be side-effect free.
  */
-#define MAX(_a_, _b_)	((_a_) > (_b_) ? (_a_) : (_b_))
-#define MIN(_a_, _b_)	((_a_) < (_b_) ? (_a_) : (_b_))
-#define ABS(_a_)	((_a_) >= 0 ? (_a_) : -(_a_))
+#define MAX(a_, b_)	((a_) > (b_) ? (a_) : (b_))
+#define MIN(a_, b_)	((a_) < (b_) ? (a_) : (b_))
+#define ABS(a_)	((a_) >= 0 ? (a_) : -(a_))
 
 /*
  * IN_RANGE() --Test if a value is within a specified range.
@@ -58,26 +92,26 @@
  * See Also:
  * http://floating-point-gui.de/errors/comparison/
  */
-#define IN_RANGE(_a_, _min_, _max_) ((_a_)-(_min_) < (_max_)-(_min_))
-#define FEQUAL(_x_, _y_, _err_) \
-    (ABS((ABS(_x_)-ABS(_y_)))/(ABS(_x_)+ABS(_y_)+(_err_)) <= (_err_))
+#define IN_RANGE(a_, min_, max_) ((a_)-(min_) < (max_)-(min_))
+#define FEQUAL(x_, y_, err_) \
+    (ABS((ABS(x_)-ABS(y_)))/(ABS(x_)+ABS(y_)+(err_)) <= (err_))
 
 /*
  * NEL() --Return the number of elements in an array.
  * NEW() --Allocate space for some items of a specified type.
  */
-#define NEL(_array_)	(sizeof _array_ / sizeof _array_[0])
-#define NEW(_type_, _nel_)	calloc((_nel_), sizeof(_type_))
+#define NEL(array_)	(sizeof array_ / sizeof array_[0])
+#define NEW(type_, nel_)	calloc((nel_), sizeof(type_))
 
 /*
  * SYS_RETRY() --a lame but portable version of GNU's TEMP_FAILURE_RETRY.
  */
 #ifdef TEMP_FAILURE_RETRY
-#define SYS_RETRY(_result_, _expression_) \
-    _result_ = TEMP_FAILURE_RETRY(_expression_)
+#define SYS_RETRY(result_, expression_) \
+    _result_ = TEMP_FAILURE_RETRY(expression_)
 #else
-#define SYS_RETRY(_result_, _expression_) \
-    do { _result_ = (_expression_); } \
+#define SYS_RETRY(result_, expression_) \
+    do { _result_ = (expression_); } \
     while (_result_ == -1 && (errno == EINTR || errno == EAGAIN))
 #endif /* TEMP_FAILURE_RETRY */
 #endif /* XTD_H */
