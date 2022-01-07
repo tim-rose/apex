3af4abee5d864328b4fa6eeb351dbb85fba7a13e
Tim Rose <tim.rose@acm.org>
2022-01-07 14:50:21 +1100

    added compare functions for C basic types

--- libxtd/misc/compare.c
+++ libxtd/misc/compare.c
@@ -15,6 +15,31 @@ int int_cmp(const void *v_1, const void *v_2)
     return int_1 - int_2;
 }
 
+int long_cmp(const void *v_1, const void *v_2)
+{
+    int long_1 = * (int *) v_1;
+    int long_2 = * (int *) v_2;
+
+    return long_1 - long_2;
+}
+
+int float_cmp(const void *v_1, const void *v_2)
+{
+    int float_1 = * (int *) v_1;
+    int float_2 = * (int *) v_2;
+
+    return float_1 - float_2;
+}
+
+int double_cmp(const void *v_1, const void *v_2)
+{
+    int double_1 = * (int *) v_1;
+    int double_2 = * (int *) v_2;
+
+    return double_1 - double_2;
+}
+
+
 int strp_cmp(const void *v_1, const void *v_2)
 {
     char *str_1 = * (char **) v_1;
--- libxtd/misc/compare.h
+++ libxtd/misc/compare.h
@@ -21,6 +21,9 @@ extern "C"
     typedef int (*CompareProc)(const void *data, const void *key);
 
     int int_cmp(const void *v_1, const void *v_2);
+    int long_cmp(const void *v_1, const void *v_2);
+    int float_cmp(const void *v_1, const void *v_2);
+    int double_cmp(const void *v_1, const void *v_2);
     int strp_cmp(const void *v_1, const void *v_2);
 
 #ifdef __cplusplus
