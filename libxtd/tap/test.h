adee534ae07afb5137d51c480103ac4d1c9e702d
Tim Rose <tim.rose@acm.org>
2023-07-11 15:10:17 +1000

    feat(test): refactored test macros.

--- libxtd/tap/test.h
+++ libxtd/tap/test.h
@@ -8,29 +8,29 @@
 #ifndef TEST_H
 #define TEST_H
 
-#include <xtd/tap.h>
 #include <xtd.h>                       /* for FEQUAL */
 #include <xtd/estring.h>                   /* for strequiv */
+#include <xtd/tap.h>
 
 #define ptr_eq(have_, expected_, ...)          \
-    (ok(((char *) have_)-(((char *) expected_)) == 0, __VA_ARGS__) ? 1 : \
-        (diag("%10s: 0x%p", "got", (have_)), \
-        diag("%10s: 0x%p", "expected", (expected_)), 0))
+    (ok(((void *) have_) == (((void *) expected_)), __VA_ARGS__) ? 1 : \
+        (diag("%10s: 0x%p", "got", STR_OR_NULL(have_)), \
+        diag("%10s: 0x%p", "expected", STR_OR_NULL(expected_)), 0))
 
-#define int_eq(have_, expected_, int_fmt_, ...)          \
+#define number_eq(have_, expected_, number_fmt_, ...)          \
     (ok((have_) == (expected_), __VA_ARGS__) ? 1 :      \
-        (diag("%10s: " int_fmt_, "got", (have_)), \
-         diag("%10s: " int_fmt_, "expected", (expected_)), 0))
+        (diag("%10s: " number_fmt_, "got", (have_)), \
+         diag("%10s: " number_fmt_, "expected", (expected_)), 0))
 
 #define float_eq(have_, expected_, float_fmt_, _tolerance, ...)             \
     (ok(FEQUAL((have_), (expected_), (_tolerance)) == 0, __VA_ARGS__) ? 1 : \
         (diag("%10s: " float_fmt_, "got", (have_)), \
          diag("%10s: " float_fmt_, "expected", (expected_)), 0))
 
-#define ok_number(have_, cmp_, expected_, int_fmt_, ...)    \
-    (ok((have_) cmp_ (expected_), __VA_ARGS__) ? 1 :      \
-        (diag("%10s: " int_fmt_, "got", (have_)), \
-         diag("%10s: a value %s " int_fmt_, "expected", #cmp_, (expected_)), 0))
+#define ok_number(have_, op_, expected_, number_fmt_, ...)    \
+    (ok((have_) op_ (expected_), __VA_ARGS__) ? 1 :      \
+        (diag("%10s: " number_fmt_, "got", (have_)), \
+         diag("%10s: a value %s " number_fmt_, "expected", #op_, (expected_)), 0))
 
 #define string_eq(have_, expected_, ...)                          \
     (ok(strequiv((have_), (expected_)) == 0, __VA_ARGS__) ? 1 :   \
@@ -46,12 +46,12 @@
     string_eq(struct_.field_, expected_, \
         #struct_ "." #field_ ": " __VA_ARGS__)
 
-#define int_field_eq(struct_, field_, expected_, int_fmt_, ...)  \
-    int_eq(struct_.field_, expected_, int_fmt_, \
+#define number_field_eq(struct_, field_, expected_, number_fmt_, ...)  \
+    number_eq(struct_.field_, expected_, number_fmt_, \
         #struct_ "." #field_ ": "__VA_ARGS__)
 
-#define ok_number_field(struct_, field_, cmp_, expected_, int_fmt_, ...)    \
-    ok_number(struct_.field_, cmp_, expected_, int_fmt_,                    \
+#define ok_number_field(struct_, field_, cmp_, expected_, number_fmt_, ...)    \
+    ok_number(struct_.field_, cmp_, expected_, number_fmt_,                    \
         #struct_ "." #field_ ": "__VA_ARGS__)
 
 #endif                                 /* TEST_H */
