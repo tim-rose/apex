aa4e79e9c0e4dd9fa3e637d5446bc86c680f2164
Tim Rose <tim.rose@acm.org>
2025-01-21 09:45:23 +1100

    feature: more detailed binsearch tests

--- test/test-binsearch.c
+++ test/test-binsearch.c
@@ -3,22 +3,32 @@
  */
 #include <string.h>
 
-#include <apex/tap.h>
+#include <apex/test.h>
 #include <apex/search.h>
 
 int vector[] = { 0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20 };
 
-int main(void)
+static void test_zero_size_table(void)
 {
-    size_t i, n, key = 0;
+    int key = 0;
     int slot;
     bool status;
 
-    plan_tests(17);
-
     slot = binsearch(&key, vector, 0, sizeof(vector[0]),
                      (CompareProc) int_cmp, &status);
-    ok(!status && slot == 0, "zero-size table");
+    number_eq(status, 0, "%d", "zero-size table: status is 0");
+    number_eq(slot, 0, "%d", "zero-size table: slot is 0");
+}
+
+int main(void)
+{
+    int i, n, key = 0;
+    int slot;
+    bool status;
+
+    plan_tests(36);
+
+    test_zero_size_table();
 
     for (n = 0; n <= 10; n += 5)
     {
@@ -27,16 +37,20 @@ int main(void)
             key = i;
             slot = binsearch(&key, vector, n, sizeof(vector[0]),
                              (CompareProc) int_cmp, &status);
-            ok(status &&
-               slot == key / 2, "find key %d in vector[%d]", key, n);
+            ok_number(status, !=, 0, "%d", "find key %d in vector[%d]: status", key, n);
+            ok_number(slot, ==, key/2, "%d", "find key %d in vector[%d]: slot", key, n);
 
             key = i + 1;
             slot = binsearch(&key, vector, n, sizeof(vector[0]),
                              (CompareProc) int_cmp, &status);
-            ok(!status && slot == key / 2 + 1,
-               "find slot for key %d in vector[%d]", key, n);
+            ok_number(status, ==, 0, "%d", "failed key %d in vector[%d]: status", key, n);
+            ok_number(slot, ==, key/2+1, "%d", "find key %d in vector[%d]: slot", key, n);
         }
     }
-    /* TODO: test for search failures (head, mid, tail) */
+    key = -1;
+    slot = binsearch(&key, vector, 10, sizeof(vector[0]),
+                     (CompareProc) int_cmp, &status);
+    ok_number(status, ==, 0, "%d", "failed key %d in vector[%d]: status", key, 10);
+    ok_number(slot, ==, 0, "%d", "find key %d in vector[%d]: slot", key, 10);
     return exit_status();
 }
