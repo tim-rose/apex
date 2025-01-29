11f7401ffa1e44a77e3a96542c22ee6d2606cdbf
Tim Rose <tim.rose@acm.org>
2025-01-30 10:09:14 +1100

    refactor: modernise and cleanup, use test.h

--- test/test-getopts.c
+++ test/test-getopts.c
@@ -7,9 +7,7 @@
  * test_str_int()    --Test the behaviour of str_int().
  * test_str_int16()  --Test the behaviour of str_int16().
  * test_str_int16()  --Test the behaviour of str_int16().
- * array_equal()     --Test if two integer arrays are equal.
  * test_str_list()   --Test integer list parsing.
- * str_array_equal() --Test if two integer arrays are equal.
  * test_str_list()   --Test integer list parsing.
  *
  */
@@ -20,6 +18,7 @@
 #include <apex/estring.h>
 #include <apex/strparse.h>
 #include <apex/getopts.h>
+#include <apex/compare.h>
 
 typedef struct Opt
 {
@@ -30,21 +29,23 @@ typedef struct Opt
 static Opt null_opt = {.a = 0 };
 
 
-static void test_getopts(void);
+static void test_empty_args(void);
+static void test_all_options(void);
+static void test_partial_options(void);
 static void test_str_inet4(void);
 static void test_str_int(void);
 static void test_str_int16(void);
 static void test_str_double(void);
-static int array_equal(size_t n, int a[], int b[]);
 static void test_str_list(void);
-static int str_array_equal(size_t n, char *a[], char *b[]);
 static void test_str_str_list(void);
 
 int main(void)
 {
     plan_tests(53);
 
-    test_getopts();
+    test_empty_args();
+    test_all_options();
+    test_partial_options();
     test_str_inet4();
     test_str_int();
     test_str_int16();
@@ -56,37 +57,44 @@ int main(void)
 }
 
 /*
- * test_getopts() --Test the behaviour of getopts().
+ * test_empty_args() --Test ability to parse/process an empty argv[].
+ */
+static void test_empty_args(void)
+{
+    Opt opt = null_opt;
+    const char *args[] = { "test" };
+
+    optind = 1;
+    ok(getopts(NEL(args), args, "a:b:cd",
+               &opt.a, &opt.b, &opt.c, &opt.d), "empty args");
+}
+
+/*
+ * test_all_options() --Test ability to parse/process all options.
+ */
+static void test_all_options(void)
+{
+    Opt opt = null_opt;
+    const char *args[] = { "test", "-a", "x", "-b", "y", "-c", "-d" };
+
+    optind = 1;
+    ok(getopts(NEL(args), args, "a:b:cd",
+               &opt.a, &opt.b, &opt.c, &opt.d), "can parse all options");
+    ok(strcmp(opt.a, "x") == 0
+       && strcmp(opt.b, "y") == 0
+       && opt.c && opt.d, "can process all values (all options)");
+    /* TODO: test for failure on unspecified options */
+}
+
+/*
+ * test_partial_options() --Test ability to parse/process some options only.
  *
  * Remarks:
  * Note that we have to set optind to reset getopt()'s behaviour
  * between tests.
  */
-static void test_getopts(void)
+static void test_partial_options(void)
 {
-    do
-    {
-        Opt opt = null_opt;
-        const char *args[] = { "test" };
-
-        ok(getopts(NEL(args), args, "a:b:cd",
-                   &opt.a, &opt.b, &opt.c, &opt.d), "getopts(): empty args");
-    }
-    while (0);
-
-    optind = 1;
-    do
-    {
-        Opt opt = null_opt;
-        const char *args[] = { "test", "-a", "x", "-b", "y", "-c", "-d" };
-
-        ok(getopts(NEL(args), args, "a:b:cd",
-                   &opt.a, &opt.b, &opt.c, &opt.d), "getopts(): all options");
-        ok(strcmp(opt.a, "x") == 0
-           && strcmp(opt.b, "y") == 0
-           && opt.c && opt.d, "getopts(): values (all options)");
-    }
-    while (0);
 
     optind = 1;
     do
@@ -99,7 +107,7 @@ static void test_getopts(void)
            "getopts(): partial options");
         ok(strcmp(opt.a, "x") == 0
            && opt.b == NULL
-           && opt.c && !opt.d, "getopts(): values (partial options)");
+           && opt.c && !opt.d, "can parse subset of options");
     }
     while (0);
     optind = 1;
@@ -111,7 +119,7 @@ static void test_getopts(void)
         getopts(NEL(args), args, "a:b:cd", &opt.a, &opt.b, &opt.c, &opt.d);
         ok(strcmp(opt.a, "x") == 0
            && strcmp(opt.b, "bar") == 0
-           && opt.c && opt.d, "getopts(): default values (partial options)");
+           && opt.c && opt.d, "subset options not parsed remain unaltered");
     }
     while (0);
 }
@@ -137,7 +145,7 @@ static void test_str_inet4(void)
     ok(str_inet4_address("1.2.3.4/16", &addr, NULL)
        && addr == 0x01020304, "str_inet(): IP address, mask ignored");
     ok(!str_inet4_address("localhost", &addr, NULL),
-       "str_inet(): name resolution not supported");
+       "str_inet(): cannot parse domain names");
 }
 
 /*
@@ -152,6 +160,7 @@ static void test_str_int(void)
     ok(str_int("-123", &value) && value == -123, "str_int(): negative value");
     ok(str_int("0xff", &value) && value == 0xff, "str_int(): hex value");
     ok(!str_int("foobar", &value), "str_int(): bad value");
+    /* TODO: test min/max values */
 }
 
 /*
@@ -166,6 +175,7 @@ static void test_str_int16(void)
     ok(str_int16("-123", &hvalue)
        && hvalue == -123, "str_int16(): negative value");
     ok(!str_int16("foobar", &hvalue), "str_int16(): bad value");
+    /* TODO: test min/max values */
 }
 
 /*
@@ -185,27 +195,39 @@ static void test_str_double(void)
     ok(!str_double("foobar", &dvalue), "str_double(): bad value");
 }
 
+#ifdef SPRINT_INT_ARRAY
 /*
- * array_equal() --Test if two integer arrays are equal.
+ * sprint_int_array() --Print an array of integers.
  *
- * Parameters:
- * n    --the number of elements in each array
- * a, b --the two arrays to compare
- *
- * Returns: (int)
- * Success: 1; Failure: 0.
+ * Remarks:
+ * Uh, I don't have a use for this yet, maybe one day.
+ * Part of the design is to allow for two calls of the
+ * function to not trash each other's output.
  */
-static int array_equal(size_t n, int a[], int b[])
+static char *sprint_int_array(const int array[], size_t n_el)
 {
-    for (size_t i = 0; i < n; ++i)
+    static char text_buffer[2][100];
+    static int slot;
+    char *base = text_buffer[slot];
+    char *text = base;
+    const char *delimiter = ", ";
+
+    slot = (slot+1) % 2;
+    strcpy(text, "[");
+    text += 1;
+
+    for (int i=0; i<n_el; ++i)
     {
-        if (a[i] != b[i])
+        if (i != 0)
         {
-            return 0;
+            text += sprintf(text, "%s", delimiter);
         }
+        text += sprintf(text, "%d", array[i]);
     }
-    return 1;
+    strcpy(text, "]");
+    return base;
 }
+#endif /* SPRINT_INT_ARRAY */
 
 /*
  * test_str_list() --Test integer list parsing.
@@ -217,51 +239,34 @@ static void test_str_list(void)
 
     ok(str_int_list("1", NEL(opt_value), opt_value),
        "str_list: simple value parse");
-    ok(array_equal(1, value, opt_value), "str_list: simple value results");
+    number_eq(array_cmp(value, opt_value, 1, sizeof(value[0]), int_cmp), 0, "%d",
+              "str_list: simple value results");
 
     ok(str_int_list("1,2", NEL(opt_value), opt_value),
        "str_list: two values parse");
-    ok(array_equal(2, value, opt_value), "str_list: two value results");
+    ok(array_cmp(value, opt_value, 2, sizeof(value[0]), int_cmp) == 0,
+       "str_list: two value results");
 
     ok(str_int_list("1,2,3,4", NEL(opt_value), opt_value),
        "str_list: four values parse");
-    ok(array_equal(4, value, opt_value), "str_list: four value results");
+    ok(array_cmp(value, opt_value, 4, sizeof(value[0]), int_cmp) == 0,
+       "str_list: four value results");
 
     ok(str_int_list("1-4", NEL(opt_value), opt_value),
        "str_list: simple range");
-    ok(array_equal(4, value, opt_value), "str_list: simple range results");
+    ok(array_cmp(value, opt_value, 4, sizeof(value[0]), int_cmp) == 0,
+       "str_list: simple range results");
 
     ok(str_int_list("1-4,5-10", NEL(opt_value), opt_value),
        "str_list: list of ranges");
-    ok(array_equal(10, value, opt_value), "str_list: list of range results");
+    ok(array_cmp(value, opt_value, 10, sizeof(value[0]), int_cmp) == 0,
+       "str_list: list of range results");
 
     ok(!str_int_list("bogus", NEL(value), value), "str_list: bogus list");
     ok(!str_int_list("10-1", NEL(value), value), "str_list: bad range");
     ok(!str_int_list("1,2,3,4,5,6,7,8,9,10,11,12", NEL(value), value),
        "str_list: list overflow");
     ok(!str_int_list("1-100", NEL(value), value), "str_list: range overflow");
-}
-
-/*
- * str_array_equal() --Test if two integer arrays are equal.
- *
- * Parameters:
- * n    --the number of elements in each array
- * a, b --the two arrays to compare
- *
- * Returns: (int)
- * Success: 1; Failure: 0.
- */
-static int str_array_equal(size_t n, char *a[], char *b[])
-{
-    for (size_t i = 0; i < n; ++i)
-    {
-        if (strequiv(a[i], b[i]) != 0)
-        {
-            return 0;
-        }
-    }
-    return 1;
 }
 
 /*
@@ -276,33 +281,33 @@ static void test_str_str_list(void)
 
     ok(str_str_list(strcpy(text, "a"), NEL(opt_value), opt_value) == 1,
        "%s: simple value parse", func);
-    ok(str_array_equal(1, value, opt_value), "%s: simple value results",
-       func);
+    ok(array_cmp(value, opt_value, 1, sizeof(value[0]), strp_cmp) == 0,
+       "%s: simple value results", func);
 
     ok(str_str_list(strcpy(text, "a,b"), NEL(opt_value), opt_value) == 2,
        "%s: ',' delimiter", func);
-    ok(str_array_equal(2, value, opt_value), "%s: ',' delimiter results",
-       func);
+    ok(array_cmp(value, opt_value, 2, sizeof(value[0]), strp_cmp) == 0,
+       "%s: ',' delimiter results", func);
 
     ok(str_str_list(strcpy(text, "a b"), NEL(opt_value), opt_value) == 2,
        "%s: ' ' delimiter", func);
-    ok(str_array_equal(2, value, opt_value), "%s: ' ' delimiter results",
-       func);
+    ok(array_cmp(value, opt_value, 2, sizeof(value[0]), strp_cmp) == 0,
+       "%s: ' ' delimiter results", func);
 
     ok(str_str_list(strcpy(text, "a b"), NEL(opt_value), opt_value) == 2,
        "%s: ','+' ' delimiter", func);
-    ok(str_array_equal(2, value, opt_value), "%s: ','+' ' delimiter results",
-       func);
+    ok(array_cmp(value, opt_value, 2, sizeof(value[0]), strp_cmp) == 0,
+       "%s: ','+' ' delimiter results", func);
 
     ok(str_str_list(strcpy(text, " a b"), NEL(opt_value), opt_value) == 2,
        "%s: leading space", func);
-    ok(str_array_equal(2, value, opt_value), "%s: leading space results",
-       func);
+    ok(array_cmp(value, opt_value, 2, sizeof(value[0]), strp_cmp) == 0,
+       "%s: leading space results", func);
 
     ok(str_str_list(strcpy(text, "a b "), NEL(opt_value), opt_value) == 2,
        "%s: trailing space", func);
-    ok(str_array_equal(2, value, opt_value), "%s: trailing space results",
-       func);
+    ok(array_cmp(value, opt_value, 2, sizeof(value[0]), strp_cmp) == 0,
+       "%s: trailing space results", func);
     ok(str_str_list(strcpy(text, "a,b"), 1, opt_value) == 0,
        "%s: overflow", func);
 }
