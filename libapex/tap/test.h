2b250b510075d2dc7083b35c476af1a21e5f4b63
Tim Rose <tim.rose@acm.org>
2025-03-12 18:23:08 +1100

    feature: add ok_ptr()

--- libapex/tap/test.h
+++ libapex/tap/test.h
@@ -15,25 +15,25 @@
 #include <apex/estring.h>               /* for strequiv */
 #include <apex/tap.h>
 
-#define ptr_eq(have_, expected_, ...)          \
-    (ok(((void *) have_) == (((void *) expected_)), __VA_ARGS__) ? 1 : \
+#define ok_ptr(have_, op_, expected_, ...)          \
+    (ok(((void *) have_) op_ ((void *) expected_), __VA_ARGS__) ? 1 :      \
         (diag("%10s: 0x%p", "got", (const char *) have_), \
         diag("%10s: 0x%p", "expected", (const char *) expected_), 0))
+#define ptr_eq(have_, expected_, ...)  \
+    ok_ptr(have_, ==, expected_, __VA_ARGS__)
 
-#define number_eq(have_, expected_, number_fmt_, ...)          \
-    (ok((have_) == (expected_), __VA_ARGS__) ? 1 :      \
-        (diag("%10s: " number_fmt_, "got", (have_)), \
-         diag("%10s: " number_fmt_, "expected", (expected_)), 0))
-
-#define float_eq(have_, expected_, float_fmt_, _tolerance, ...)             \
-    (ok(FEQUAL((have_), (expected_), (_tolerance)) == 0, __VA_ARGS__) ? 1 : \
-        (diag("%10s: " float_fmt_, "got", (have_)), \
-         diag("%10s: " float_fmt_, "expected", (expected_)), 0))
+#define number_eq(have_, expected_, number_fmt_, ...) \
+    ok_number(have_, ==, expected_, number_fmt_, __VA_ARGS__)
 
 #define ok_number(have_, op_, expected_, number_fmt_, ...)    \
     (ok((have_) op_ (expected_), __VA_ARGS__) ? 1 :      \
         (diag("%10s: " number_fmt_, "got", (have_)), \
          diag("%10s: a value %s " number_fmt_, "expected", #op_, (expected_)), 0))
+
+#define float_eq(have_, expected_, float_fmt_, _tolerance, ...)             \
+    (ok(FEQUAL((have_), (expected_), (_tolerance)) == 0, __VA_ARGS__) ? 1 : \
+        (diag("%10s: " float_fmt_, "got", (have_)), \
+         diag("%10s: " float_fmt_, "expected", (expected_)), 0))
 
 #define string_eq(have_, expected_, ...)                          \
     (ok(strequiv((have_), (expected_)) == 0, __VA_ARGS__) ? 1 :   \
